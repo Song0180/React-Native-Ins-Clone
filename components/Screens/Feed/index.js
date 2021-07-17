3f561b3fee35b60864b3194869c68f31969d38f2
song0180 <song0180@e.ntu.edu.sg>
2021-07-17 14:37:31 +0800

    add feed posts

--- components/Screens/Feed/index.js
+++ components/Screens/Feed/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import * as React from 'react';
+import { FlatList, View, Text, Image } from 'react-native';
+
+import { useUserInteractStore } from '../../../shared/zustand/userInteractions';
+import { styles } from './style';
 
 export default function Feed() {
+  const { users, following, usersLoaded } = useUserInteractStore();
+
+  const [feedPosts, setFeedPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    let feedPosts = [];
+    if (usersLoaded === following.length) {
+      for (let i = 0; i < following.length; i++) {
+        const user = users.find(user => user.uid === following[i]);
+        if (user !== undefined) {
+          feedPosts = [...feedPosts, ...user.posts];
+        }
+      }
+      feedPosts.sort(function (x, y) {
+        return x.creation - y.creation;
+      });
+      setFeedPosts(feedPosts);
+    }
+  }, [usersLoaded]);
+
   return (
-    <View>
-      <Text>Feed</Text>
+    <View style={styles.container}>
+      <View style={styles.containerGallery}>
+        <FlatList
+          numColumns={1}
+          horizontal={false}
+          data={feedPosts}
+          renderItem={({ item }) => (
+            <View style={styles.containerImage}>
+              <Text>{item.user.name}</Text>
+              <Image style={styles.image} source={{ uri: item.downloadUrl }} />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
---
+++ components/Screens/Feed/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  containerGallery: {
+    flex: 1,
+  },
+  containerImage: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    aspectRatio: 1 / 1,
+  },
+});
